3074f3c2276e8707863986afb0f67617b7a68979
Ikhlass Mastour <ikhlass.mastour@mdsoft-int.com>
2022-09-27 15:30:11 +0100

    input

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Button, Card, Form} from "react-bootstrap"
-import Spinner from 'react-bootstrap/Spinner'
+// import Spinner from 'react-bootstrap/Spinner'
 import "../dashboard/style.css"
 import { CFormInput, CFormLabel, CInputGroupText } from '@coreui/react'
 
 const Todo = ({todo, removeTodo, markTodo, i}) => {
+  
         
   return (
     <div className='todo'>
     <span>{todo}</span>
-    <Spinner animation="border" variant="primary" size="sm" />
+    {/* <Spinner animation="border" variant="primary" size="sm" /> */}
     <div>
         <button variant="outline-success" className='action-btn' onClick={() => markTodo(i)}>Done</button>{' '}
         <button variant="outline-danger" className='action-btn' onClick={() => removeTodo(i)}>Rejected</button>
@@ -30,34 +31,34 @@ const Todo = ({todo, removeTodo, markTodo, i}) => {
         <Form onSubmit={handleSubmit}> 
         <CInputGroupText>
           <CFormLabel><b>Add Todo</b></CFormLabel>
-          <CFormInput type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Type your task" />
+          <CFormInput type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="eg: shopping etc.." required/>
         </CInputGroupText>
         <Button variant="primary mb-3" type="submit">
           Submit
         </Button>
       </Form>
       );
-    }
+      function Todos() {
+        const [todos, setTodos] = React.useState([]);
+      
+          const addTodo = text =>{
+            const newTodos = [...todos, { text }];
+            setTodos(newTodos)
+          }
+      
+          const markTodo = i => {
+            const newTodos = [...todos];
+            newTodos[i].isDone = true;
+            setTodos(newTodos);
+          };
+      
+          const removeTodo = i =>{
+            const newTodos = [...todos];
+            newTodos.splice(i, 1);
+            setTodos(newTodos)
+          };
 
-function Todos() {
-  const [todos, setTodos] = React.useState([]);
 
-    const addTodo = text =>{
-      const newTodos = [...todos, { text }];
-      setTodos(newTodos)
-    }
-
-    const markTodo = i => {
-      const newTodos = [...todos];
-      newTodos[i].isDone = true;
-      setTodos(newTodos);
-    };
-
-    const removeTodo = i =>{
-      const newTodos = [...todos];
-      newTodos.splice(i, 1);
-      setTodos(newTodos)
-    };
 return (
   <div className='todoList'>
       <div className='container'>
@@ -82,5 +83,6 @@ return (
   </div>
 )
   }
-}
+    }
+  }
 export default Todo
