2a96614d9b2ae5de1e5fa352e01be689b2496d46
Ikhlass Mastour <ikhlass.mastour@mdsoft-int.com>
2022-09-28 12:03:38 +0100

    dashboard tasks working

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,88 +1,86 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Button, Card, Form} from "react-bootstrap"
-// import Spinner from 'react-bootstrap/Spinner'
-import "../dashboard/style.css"
-import { CForm, CFormInput, CFormLabel, CInputGroupText } from '@coreui/react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Card, Form} from "react-bootstrap";
+import Spinner from 'react-bootstrap/Spinner';
 
-const Todo = ({todo, removeTodo, markTodo, i}) => {
-  
-        
-  return (
-    <div className='todo'>
-    <span>{todo}</span>
-    {/* <Spinner animation="border" variant="primary" size="sm" /> */}
-    <div>
-        <button variant="outline-success" className='action-btn' onClick={() => markTodo(i)}>Done</button>{' '}
-        <button variant="outline-danger" className='action-btn' onClick={() => removeTodo(i)}>Rejected</button>
-    </div>
-</div>
+
+function Todo ({todo, removeTodo, markTodo, i}){
+    return(
+        <div className='todo'>
+            <span>{todo.text}</span>
+            <Spinner animation="border" variant="primary" size="sm" />
+            <div>
+                <button variant="outline-success" onClick={() => markTodo(i)}>Done</button>{' '}
+                <button variant="outline-danger" onClick={() => removeTodo(i)}>Rejected</button>
+            </div>
+        </div>
     )
-    function FormTodo({ addTodo }) {
-      const [value, setValue] = React.useState("");
-  
-      const handleSubmit = e => {
-      e.preventDefault();
-      if (!value) return;
-      addTodo(value);
-      setValue("");
+}
+       function FormTodo({ addTodo }) {
+        const [value, setValue] = React.useState("");
+    
+        const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+        };
+        return (
+          <Form onSubmit={handleSubmit}> 
+          <Form.Group>
+            <Form.Label><b>Add Todo</b></Form.Label>
+            <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Type your task" />
+          </Form.Group>
+          <Button variant="primary mb-3" type="submit">
+            Submit
+          </Button>
+        </Form>
+        );
+      }
+
+function Todos() {
+    const [todos, setTodos] = React.useState([]);
+
+      const addTodo = text =>{
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos)
+      }
+
+      const markTodo = i => {
+        const newTodos = [...todos];
+        newTodos[i].isDone = true;
+        setTodos(newTodos);
       };
-      return (
-        <CForm onSubmit={handleSubmit}>
-        <CInputGroupText>
-          <CFormLabel><b>Add Todo</b></CFormLabel>
-          <CFormInput type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="eg: shopping etc.." required/>
-        </CInputGroupText>
-        <Button variant="primary mb-3" type="submit">
-          Submit
-        </Button>
-      </CForm>
-      );
-       function Todos() {
-        const [todos, setTodos] = React.useState([]);
-      
-          const addTodo = text =>{
-            const newTodos = [...todos, { text }];
-            setTodos(newTodos)
-          }
-      
-          const markTodo = i => {
-            const newTodos = [...todos];
-            newTodos[i].isDone = true;
-            setTodos(newTodos);
-          };
-      
-          const removeTodo = i =>{
-            const newTodos = [...todos];
-            newTodos.splice(i, 1);
-            setTodos(newTodos)
-          };
 
+      const removeTodo = i =>{
+        const newTodos = [...todos];
+        newTodos.splice(i, 1);
+        setTodos(newTodos)
+      };
+  return (
+    <div className='todoList'>
+        <div className='container'>
+            <FormTodo addTodo={addTodo} />
+            <div>
+                {todos.map((todo, i) =>(
+                    <Card>
+                        <Card.Body>
+                            <Todo
+                            key={i}
+                            i = {i}
+                            todo={todo}
+                            markTodo={markTodo}
+                            removeTodo={removeTodo}
+                            />
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+        </div>
 
-return (
-  <div className='todoList'>
-      <div className='container'>
-          <FormTodo addTodo={addTodo} />
-          <div>
-              {todos.map((todo, i) =>(
-                  <Card>
-                      <Card.Body>
-                          <Todo
-                          key={i}
-                          i = {i}
-                          todo={todo}
-                          markTodo={markTodo}
-                          removeTodo={removeTodo}
-                          />
-                      </Card.Body>
-                  </Card>
-              ))}
-          </div>
-      </div>
+    </div>
+  )
+}
 
-  </div>
-)
-  }
-    }
-  }
-export default Todo
+        export default Todos
+
