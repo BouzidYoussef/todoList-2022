8e2078ea3ec35a59186faff223cdd3fb0d29a956
Ikhlass Mastour <ikhlass.mastour@mdsoft-int.com>
2022-10-20 12:29:52 +0100

    table adjust

--- src/views/pages/tasks/Tasks.js
+++ src/views/pages/tasks/Tasks.js
@@ -1,66 +1,13 @@
 import React, { useState } from "react";
 import Todos from "src/views/dashboard/Dashboard";
 import jsonData from './data.json';
-import Switch from '@mui/material/Switch';
-import { styled } from '@mui/material/styles';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import {CTable, CTableHead, CTableRow, CTableHeaderCell, CTableBody, CTableDataCell} from '@coreui/react';
+import { Button} from 'react-bootstrap';
+
 
 
 function TableData() {
 
-
-  const IOSSwitch = styled((props) => (
-    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-  ))(({ theme }) => ({
-    width: 42,
-    height: 26,
-    padding: 0,
-    '& .MuiSwitch-switchBase': {
-      padding: 0,
-      margin: 2,
-      transitionDuration: '300ms',
-      '&.Mui-checked': {
-        transform: 'translateX(16px)',
-        color: '#fff',
-        '& + .MuiSwitch-track': {
-          backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
-          opacity: 1,
-          border: 0,
-        },
-        '&.Mui-disabled + .MuiSwitch-track': {
-          opacity: 0.5,
-        },
-      },
-      '&.Mui-focusVisible .MuiSwitch-thumb': {
-        color: '#33cf4d',
-        border: '6px solid #fff',
-      },
-      '&.Mui-disabled .MuiSwitch-thumb': {
-        color:
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[100]
-            : theme.palette.grey[600],
-      },
-      '&.Mui-disabled + .MuiSwitch-track': {
-        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      boxSizing: 'border-box',
-      width: 22,
-      height: 22,
-    },
-    '& .MuiSwitch-track': {
-      borderRadius: 26 / 2,
-      backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-      opacity: 1,
-      transition: theme.transitions.create(['background-color'], {
-        duration: 500,
-      }),
-    },
-  }));
-    
   const [tasksData, setTaskData] = useState(jsonData);
   const current = new Date();
   const moment = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
@@ -68,24 +15,38 @@ function TableData() {
   const tableRows = tasksData.map((info, i) => {
     
     return (
-      <CTableBody>
-      <CTableRow key={i}>
+      <CTableBody color="light">
+      <CTableRow key={info.id}>
         <CTableHeaderCell scope="row">{info.id}</CTableHeaderCell>
-        <CTableDataCell>{info.user}</CTableDataCell>
-        <CTableDataCell>{info.task}</CTableDataCell>
-        <CTableDataCell>{info.description}</CTableDataCell>
-        <CTableDataCell>
+        <CTableDataCell className="row-row">{info.user}</CTableDataCell>
+        <CTableDataCell className="row-row">{info.task}</CTableDataCell>
+        <CTableDataCell className="row-row">{info.description}</CTableDataCell>
+        <CTableDataCell className="row-row">
           {date}
           <br />
           {moment}
         </CTableDataCell>
-        <FormControlLabel
-        control={<IOSSwitch sx={{ m: 1 }} />}
-      />
+        <CTableDataCell>
+        <Button className="row-row" variant="outline-success" onClick={() => markTodo(i)}>✓</Button>{' '}
+        <Button className="row-row" variant="outline-danger" onClick={() => removeTodo(i)}>✕</Button>
+        </CTableDataCell>
       </CTableRow>
     </CTableBody>
     );
   });
+
+  const markTodo = index => {
+    const newTodos = [...tasksData];
+    newTodos[index].isDone = true;
+    setTaskData(newTodos);
+  };
+
+
+  const removeTodo = index => {
+    const newTodos = [...tasksData];
+    newTodos.splice(index, 1);
+    setTaskData(newTodos);
+  };
   
   const addRows = (data) => {
     const totalTasks = tasksData.length;
@@ -96,16 +57,19 @@ function TableData() {
   };
   
   return (
-      <><CTable striped>
-      <CTableHead>
+      <CTable striped>
+      <CTableHead color="light">
         <CTableRow>
-          <CTableHeaderCell scope="col">#</CTableHeaderCell>
-          <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-          <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-          <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Id</CTableHeaderCell>
+          <CTableHeaderCell scope="col">User</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Task</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Created At</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Status</CTableHeaderCell>
         </CTableRow>
       </CTableHead>
-    </CTable><CTableBody>{tableRows}</CTableBody><Todos func={addRows} /></>
+    <CTableBody>{tableRows}</CTableBody><Todos func={addRows} />
+    </CTable> 
   );
 }
 export default TableData;
